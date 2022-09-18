63b6466d7ec7fe81e49e92e0f7ea0b4e2da877dc
Chibu <atuchibueze@gmail.com>
2022-09-18 13:19:17 -0400

    create characterItem and characterGrid components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+import Header from "./components/ui/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
 
 function App() {
-  return <div className="container">Hello</div>;
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios.get(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, []);
+  return (
+    <div className="container">
+      <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
+    </div>
+  );
 }
 
 export default App;
