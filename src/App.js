5e975870695bed2124e8430042da4c874b6b34a4
Chibu <atuchibueze@gmail.com>
2022-09-19 06:30:15 -0400

    implement spinner component

--- src/App.js
+++ src/App.js
@@ -3,27 +3,36 @@ import axios from "axios";
 import "./App.css";
 import Header from "./components/ui/Header";
 import CharacterGrid from "./components/characters/CharacterGrid";
+import Search from "./components/ui/Search";
 
 function App() {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchItems = async () => {
       const result = await axios.get(
-        `https://www.breakingbadapi.com/api/characters`
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
       );
 
-      console.log(result.data);
+      // console.log(result.data);
+
       setItems(result.data);
       setIsLoading(false);
     };
 
     fetchItems();
-  }, []);
+  }, [query]);
+
+  const queryFunction = (q) => {
+    setQuery(q);
+  };
+
   return (
     <div className="container">
       <Header />
+      <Search getQuery={queryFunction} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
